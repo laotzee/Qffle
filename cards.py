9169bf78018e3d38923c66403511337f4b1575b4
laotzee <laotzeabreu@proton.me>
2026-05-13 20:30:42 -0400

    Add fnc to lookup decks by path

--- cards.py
+++ cards.py
@@ -1,15 +1,20 @@
 import csv
+from db import Deck, db_session
+from sqlalchemy import select
 from random import shuffle
 from typing import Tuple
 
 FILE_PATH = "resources/most_used_german_words.csv"
 READ_MAX = 10000
 
+
 def create_card(text_pair: list):
     """Returns a tuple containing two strings of a card. If pair is missing
     an empty value takes its place"""
-    if len(text_pair) < 2: text_pair.append("")
+    if len(text_pair) < 2:
+        text_pair.append("")
     return (text_pair[0], text_pair[1])
+
 
 def read_cards(file):
     """Returns the contents of a csv file as X"""
@@ -19,25 +24,31 @@ def read_cards(file):
     elif file[-4:].lower() != ".csv":
         return file
 
-    with open(file) as read_file: #I need to put a guard here
-
+    with open(file) as read_file:  # I need to put a guard here
         reader = csv.reader(read_file)
-        cards = {
-            "headings": next(reader),
-            "cards": []}
+        cards = {"headings": next(reader), "cards": []}
         for row in reader:
             cards["cards"].append(create_card(row))
         return cards
 
+
 def shuffle_cards(cards):
-    """Randomize the order of the cards """
+    """Randomize the order of the cards"""
 
     if type(cards) == dict:
         shuffle(cards["cards"])
 
+
+def get_deck_by_path(search_path: str) -> Deck | None:
+    """
+    Looks up a deck by its path.
+    Returns the Deck instance if found, otherwise None.
+    """
+    stmt = select(Deck).where(Deck.path == search_path)
+    return db_session.execute(stmt).scalar_one_or_none()
+
+
 if __name__ == "__main__":
-
-
     words = read_cards(FILE_PATH)
 #    count = 0
 #    for row in words:
