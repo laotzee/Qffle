d16180a9590f71b0db316105e2681f2ddc909851
laotzee <laotzeabreu@proton.me>
2026-05-19 11:34:43 -0400

    Adjust cli to work with DeckContent

--- cli.py
+++ cli.py
@@ -1,35 +1,34 @@
 import os
 import sys
 import datetime
-from cards_bak import (
+from cards import (
     create_deck,
     get_deck_by_path,
     read_cards,
-    shuffle_cards,
     create_session,
     update_deck,
     get_best_session,
 )
-from db import Deck, Session
+from db import Deck, ContentDeck, Session
 
 
-def sort_set(s) -> list:
-    """Returns a list containing all the elements of the set sorted
-    alphabetically"""
+def sort_cards(s: set) -> list:
+    """Returns a list containing all the elements of a set of cards sorted
+    alphabetically by their question"""
     sorted_list = list(s)
-    sorted_list.sort()
+    sorted_list.sort(key=lambda card: card.question)
     return sorted_list
 
 
 def report_errors(error_cards: set):
 
     if error_cards:
-        sorted_cards = sort_set(error_cards)
+        sorted_cards = sort_cards(error_cards)
         print("Cards you may want to review for next session")
-        print("-------------------------------------")
+        print("-------------------------------------\n")
         for card in sorted_cards:
-            print(card)
-        print("-------------------------------------")
+            print(f"{card.question} -----> {card.answer}")
+        print("\n-------------------------------------")
 
 
 def compare_sessions(current_session: Session, best_session: Session):
@@ -49,46 +48,41 @@ def compare_sessions(current_session: Session, best_session: Session):
     print("-------------------------------------")
 
 
-def start_session(deck: Deck, deck_content):
+def start_session(deck: Deck, deck_content: ContentDeck):
     """Shows the user a given deck to memorize its cards"""
 
-    headings = deck_content["headings"]
-    cards = deck_content["cards"]
     points = 0
     mistakes = 0
     pending_cards = []
     error_cards = set()
-    question_header = headings[0]
-    answer_header = headings[1]
 
     start = datetime.datetime.now()
-    while cards:
+    while deck_content.cards:
         clear_screen()
         if pending_cards:
             card = pending_cards.pop()
             print("Invalid input, just use 1 or 0")
         else:
-            card = cards.pop(0)
+            card = deck_content.cards.pop(0)
         print("================================\n")
-        print(f"{question_header}\n")
-        print(f"{card[0]}\n")
+        print(f"{deck_content.question_header}\n")
+        print(f"{card.question}\n")
 
         input()
 
-        print(f"{question_header} -----> {answer_header}\n")
-        print(f"{card[0]} -----> {card[1]}\n")
+        print(f"{deck_content.question_header} -----> {deck_content.answer_header}\n")
+        print(f"{card.question} -----> {card.answer}\n")
         print("================================\n")
 
         answer = input("(1) Right (2) Wrong\n")
         if answer.isdigit() and answer == "1":
             points += 1
         elif answer.isdigit() and answer == "2":
-            cards.append(card)
+            deck_content.cards.append(card)
             error_cards.add(card)
             mistakes += 1
         else:
             pending_cards.append(card)
-            continue
 
     end = datetime.datetime.now()
 
@@ -119,13 +113,15 @@ if not file:
     file = input("Absolute path for the cards: ")
 
 deck_content = read_cards(file)
-shuffle_cards(deck_content)
-deck_len = len(deck_content["cards"])
+if not deck_content:
+    print("File must exist and have a .csv extention")
+    exit()
 
 deck = get_deck_by_path(file)
+
 if deck:
-    update_deck(deck, deck_len)
+    update_deck(deck, deck_content.deck_len)
 else:
-    deck = create_deck(file, deck_len)
+    deck = create_deck(file, deck_content.deck_len)
 
 start_session(deck, deck_content)
