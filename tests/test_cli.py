a886705cc644bb7bd7266646524e1642baf9d48c
laotzee <laotzeabreu@proton.me>
2026-05-28 11:09:11 -0400

    Refactor test for cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,11 @@
-from db import Card
 from cli import sort_cards
 
 
-def test_sort_cards():
+def test_sort_cards(mock_content_deck):
 
-    card1 = Card("What is the German word for 'Bread'?", "Das Brot")
-    card2 = Card("How many days does a year typically have?", "365")
-    test_set = {card1, card2}
+    test_set = set(mock_content_deck.cards)
 
     sorted_list = sort_cards(test_set)
 
-    assert sorted_list[0].question == card2.question
-    assert sorted_list[1].question == card1.question
+    assert sorted_list[0].question.startswith("How many")
+    assert sorted_list[1].question.startswith("What is")
