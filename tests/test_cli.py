501f02f005a6acc633232d32c81f44f8e2d14941
laotzee <laotzeabreu@proton.me>
2026-06-06 11:33:10 -0400

    Add test for session_report function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
-from cli import sort_cards
+from sqlalchemy.engine import mock
+from cli import session_report, sort_cards, session_report
 
 
 def test_sort_cards(mock_content_deck):
@@ -9,3 +10,21 @@ def test_sort_cards(mock_content_deck):
 
     assert sorted_list[0].question.startswith("How many")
     assert sorted_list[1].question.startswith("What is")
+
+
+def test_session_report(mock_sessions):
+    """Test that every piece of the report corresponds with the data inside
+    the respective sessions"""
+
+    properties_to_test = [
+        ["item_count", "Total cards:"],
+        ["errors", "Mistakes:"],
+        ["duration", "Duration:"],
+    ]
+    test_sessions = mock_sessions[:2]
+    report = session_report(test_sessions[0], test_sessions[1])
+
+    for session in test_sessions:
+        for attr_name, label in properties_to_test:
+            value = getattr(session, attr_name)
+            assert f"{label} {value}" in report
